6c26eb1fcb363022ad9358344fb0cb8e846d0a1d
Izzan Abdul <izzanabdul123@gmail.com>
2024-08-06 17:58:58 +0700

    feat: add api route

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,11 @@ use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/courses', [CourseController::class, 'popular']);
+Route::get('/courses/popular', [CourseController::class, 'popular']);
+Route::get('/courses/{id}', [CourseController::class, 'detail']);
+Route::post('/courses', [CourseController::class, 'store']);
+Route::put('/courses/{id}', [CourseController::class, 'update']);
+Route::delete('/courses/{id}', [CourseController::class, 'delete']);
 
 Route::get('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'register']);
